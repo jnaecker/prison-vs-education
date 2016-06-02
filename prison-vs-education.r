9bd9e05706fc8e257eba1e32243d8bd6978b6851
Jeffrey K Naecker <jnaecker@gmail.com>
2016-06-01 22:08:36 -0400

    switched to plotly graphics

--- prison-vs-education.r
+++ prison-vs-education.r
@@ -1,10 +1,19 @@
 library(ggplot2)
+library(plotly)
+library(htmlwidgets)
 
-data <- read.csv("data.csv")
+data <- read.csv("data.csv", stringsAsFactors=F)
 
-ggplot(data, aes(x=per.prisoner.cost, y=per.pupil.spending)) +
-  geom_point() +
-  geom_abline() + 
-  xlim(0,60000) + ylim(0,20000) + 
-  stat_smooth(method="lm") +
-  theme_minimal()
+data$ratio <- data$per.prisoner.cost/data$per.pupil.spending
+
+p <- plot_ly(data,
+        x = per.prisoner.cost, 
+        y = per.pupil.spending, 
+        text = state,
+        mode = "markers", 
+        color = ratio) %>%
+        layout(
+          xaxis = list(range = c(0, 60000)),
+          yaxis = list(range = c(0, 20000)))
+
+saveWidget(as.widget(p), "graph.html")
